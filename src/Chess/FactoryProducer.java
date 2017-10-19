25a156de5d4670698243206e75903d525942644b
Aivaras Baranauskas <aiarasb@gmail.com>
2017-10-19 20:21:22 +0300

    Abstract factory

--- src/Chess/FactoryProducer.java
+++ src/Chess/FactoryProducer.java
@@ -6,9 +6,25 @@ package Chess;
 
 public class FactoryProducer
 {
+    public final String FIGURE = "figure";
+    public final String CELL = "cell";
+    
     public AbstractFactory getFactory( String type )
     {
-        return null;
+        AbstractFactory factory;
+        
+        switch (type) {
+            case FIGURE:
+                factory = new FigureFactory();
+                break;
+            case CELL:
+                factory = new CellFactory();
+                break;
+            default:
+                throw new TypeNotPresentException(type, null);
+       }
+        
+        return factory;
     }
     
     
