3fcf4e7e55f9fcd674809a88e69ce87c77d015e7
Aivaras Baranauskas <aiarasb@gmail.com>
2017-10-19 20:24:52 +0300

    Bridge

--- src/Chess/Cell.java
+++ src/Chess/Cell.java
@@ -9,6 +9,22 @@ public abstract class Cell
     private Position position;
     
     private Figure figure;
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
     
     
 }
